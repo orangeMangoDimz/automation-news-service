75135d13da063cba3e043058c7583c0917278d39
orangeMangoDimz <hakuka123osaimo456@gmail.com>
2025-04-20 14:10:17 +0700

    feat: implement log for scrapping news
    * Implement logger for parent(LoadSelenium) and child(DetikNews) class
    * Implement decorator logging

--- news/detik.py
+++ news/detik.py
@@ -8,21 +8,35 @@ from typing import List
 from utils.type_hint import ArticleTtype
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from utils.constant import PAGE_LOAD_TIMEOUT
-from settings.decorator import retry 
+from utils.constant import PAGE_LOAD_TIMEOUT, PAGINATION_LINK_LIMIT
+from settings.decorator import retry
+from datetime import date
 
 
 class DetikNews(LoadSelenium):
-    def __init__(self, keyword: str | None) -> None:
+    def __init__(self, keyword: str) -> None:
         super().__init__()
-        self.url: str = f"https://www.detik.com/search/searchall?query={keyword}"
+        self.url: str = self._get_url(keyword)
         self.prev_url = self.url
         self.list_article_link: List[str] = []
+
+    def _get_url(self, keyword: str):
+        # Base URL for non debug mode
+        base_url: str = f"https://www.detik.com/search/searchall?query={keyword}"
+
+        # Inject applied filter with debug mode
+        formatted_current_date: str = date.today().strftime("%d/%m/%Y")
+        debug_url: str = f"https://www.detik.com/search/searchnews?query={keyword}&result_type=latest&fromdatex={formatted_current_date}&todatex={formatted_current_date}"
+
+        return base_url if self.is_debug_mode else debug_url
 
     def do_search(self) -> None:
         self.driver.get(self.url)
 
     def do_apply_filter(self) -> None:
+        if not self.is_debug_mode:
+            return
+
         # == Terbaru
         terbaru_el = self.driver.find_element(By.CSS_SELECTOR, "a[dtr-act='terbaru']")
         terbaru_el.click()
@@ -41,25 +55,29 @@ class DetikNews(LoadSelenium):
         hari_ini.click()
 
         self.prev_url = self.driver.current_url
+        self.logger.info("Success applying filter")
 
     def do_get_pagination_links(self) -> List[str]:
         # Get maximum 2 pages, probably including "Prev" or "Next" btn
         pagination_container = self.driver.find_elements(
             By.CSS_SELECTOR, ".pagination > a[dtr-evt='search result']"
-        )[:2]
-        # Get all pagination links 
+        )[:PAGINATION_LINK_LIMIT]
+        # Get all pagination links
         pagination_link_list: List[str] = [
-            href 
+            href
             for p in pagination_container
-            if p.text.lower() not in ["prev", "next"] and (href := p.get_attribute("href"))
+            if p.text.lower() not in ["prev", "next"]
+            and (href := p.get_attribute("href"))
         ]
+
+        self.logger.info(f"Found {len(pagination_link_list)} page(s)")
         return pagination_link_list
 
     @retry(exec_msg="Articles")
     def do_get_articles(self, pagination_list: List[str]) -> List[ArticleTtype]:
         article: List[ArticleTtype] = []
 
-        for page_link in pagination_list:
+        for page_num, page_link in enumerate(pagination_list):
             # Navigate to next pagination link
             self.list_article_link: List[str] = []
             self.driver.get(page_link)
@@ -71,6 +89,8 @@ class DetikNews(LoadSelenium):
             list_content = content_container.find_elements(
                 By.CSS_SELECTOR, ".list-content__item"
             )
+
+            self.logger.info(f"Getting the {page_num + 1} page...")
 
             # Get all article link for each page
             for content in list_content:
@@ -85,6 +105,7 @@ class DetikNews(LoadSelenium):
             # Navigate to all articles link and scrap them
             for idx, title_link in enumerate(self.list_article_link):
 
+                self.logger.info(f"Getting the {idx + 1} article ...")
                 self.driver.set_page_load_timeout(PAGE_LOAD_TIMEOUT)
 
                 try:
@@ -95,7 +116,9 @@ class DetikNews(LoadSelenium):
                 # Get the article title and body content
                 try:
                     title = WebDriverWait(self.driver, 10).until(
-                        EC.presence_of_element_located((By.CSS_SELECTOR, ".detail__title"))
+                        EC.presence_of_element_located(
+                            (By.CSS_SELECTOR, ".detail__title")
+                        )
                     )
                     body_container = self.driver.find_element(
                         By.CSS_SELECTOR, ".detail__body-text"
@@ -106,6 +129,9 @@ class DetikNews(LoadSelenium):
                         By.CSS_SELECTOR, ".detail__body"
                     )
                 except TimeoutException:
+                    self.logger.warning(
+                        "Timeout limit reached when getting title and body content of article!"
+                    )
                     continue
 
                 all_p = body_container.find_elements(By.TAG_NAME, "p")
@@ -122,13 +148,6 @@ class DetikNews(LoadSelenium):
                         "link": self.driver.current_url,
                     }
                 )
-
-                print("-------------")
-                print("Article info")
-                print("-------------")
-                print("Article Num  : ", idx + 1)
-                print("title        : ", title.text)
-                print("Link         : ", self.driver.current_url)
 
                 self.driver.get(self.prev_url)
 
