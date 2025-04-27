6187d1e2e759c9ee3b47bae732795bd2851a0d5f
orangeMangoDimz <hakuka123osaimo456@gmail.com>
2025-04-27 16:33:07 +0700

    ref: constat var
    * Ref new instruction, profile template, and Indonesia datetime

--- utils/constant.py
+++ utils/constant.py
@@ -1,5 +1,43 @@
 # Instruction
-SUMMARIZE_INSTRUCTION = "Ringkas artikel berikut menjadi 1 paragraf. Berikan poin - point penting nya saja dalam bentuk list"
+SUMMARIZE_INSTRUCTION = """
+[Aturan]
+- Ringkas artikel di atas menjadi point-point penting saja
+- Langsung gunakan format di bawah ini
+- Jangan tambahkan apapun di luar format di bawah
+
+[Format]
+- [Judul point]
+    - [Isi point 1]
+    - [Isi point 2]
+    - [Isi point 3]
+    dan seterusnya ...
+"""
+
+# Date
+LIST_OF_DAYS = {
+    "Monday": "Senin",
+    "Tuesday": "Selasa",
+    "Wednesday": "Rabu",
+    "Thursday": "Kamis",
+    "Friday": "Jumat",
+    "Saturday": "Sabtu",
+    "Sunday": "Minggu"
+}
+
+LIST_OF_MONTHS = {
+    "January": "Januari",
+    "February": "Februari",
+    "March": "Maret",
+    "April": "April",
+    "May": "Mei",
+    "June": "Juni",
+    "July": "Juli",
+    "August": "Agustus",
+    "September": "September",
+    "October": "Oktober",
+    "November": "November",
+    "December": "Desember"
+}
 
 # Gemini
 API_KEY_NOT_FOUND = "You need to insert Gemini API Key first!"
@@ -17,3 +55,8 @@ PAGINATION_LINK_LIMIT = 2
 # Other
 DEBUG_MODE = "Your are not setitng debug status! It will set TRUE automaticly!"
 ENV_NOT_COMPLETE = "ENV are not fully set! Please refer to the .env.example"
+
+# Profile
+AUTHOR_PROFILE = "https://github.com/orangeMangoDimz"
+DISCORD_BOT_REPO = "https://github.com/orangeMangoDimz/mango-bot"
+BACKEND_REPO = "https://github.com/orangeMangoDimz/automation-news-service"
